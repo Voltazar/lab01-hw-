f9e2969cdb5f30194fb2b08ceba12856643a06c5
Voltazar <32140020+Voltazar@users.noreply.github.com>
2017-09-28 20:41:40 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 int main()
+int main()
 {   // var 16 num 6
     cout << "enter the three-digit number x ";
     int x;
     cin >> x;
-    div_t y = div(x, 10);
-    cout << "number = " << (x % 10) * 100 + y.quot << endl << endl;
+    int y=x/10;
+    cout << "number = " << (x % 10) * 100 + y << endl << endl;
     // var 16 num 19
     x = 456;
     cout << x << endl;
     cout << "crossed out 2 digit" << endl;
-    y = div((x % 100), 10);
-    div_t k = div(x, 100);
-    cout << "number=" << y.quot * 100 + k.quot * 10 + x % 10;
+    y = (x % 100)/10;
+    cout << "number=" << y * 100 + (x/100) * 10 + x % 10;
     return 0;
 }
