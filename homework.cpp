d6f51f1f1ec268f77c328754203a397f6d001619
Voltazar <32140020+Voltazar@users.noreply.github.com>
2017-09-28 20:34:06 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -9,10 +9,10 @@ int main()
     int x;
     cin >> x;
     div_t y = div(x, 10);
-    cout << "number = " << (x % 10) * 100 + y.quot << endl<<endl;
+    cout << "number = " << (x % 10) * 100 + y.quot << endl << endl;
     // var 16 num 19
     x = 456;
-    cout << x<<endl;
+    cout << x << endl;
     cout << "crossed out 2 digit" << endl;
     y = div((x % 100), 10);
     div_t k = div(x, 100);
